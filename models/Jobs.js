e19cba427586f240813cdaaf83a5e4700282b52c
tiffanymesser <tiffanymesser15@gmail.com>
2019-09-11 19:35:26 -0400

    added onet JSON and index

---
+++ models/Jobs.js
@@ -0,0 +1,8 @@
+       cosnt mongoose = require("../jobs.json")
+       const Schema = mongoose.Schema
+       
+       
+       // "uuid": "26bc4486dfd0f60b3bb0d8d64e001800",
+        // "title": "1st Grade Teacher",
+        // "normalized_job_title": "1st grade teacher",
+        // "parent_uuid": "51cc95467c1844c7dc1d923265c023b2"
