261823862d5c6c293a8e5fb5e99f74ebc68f80c9
tiffanymesser <tiffanymesser15@gmail.com>
2019-09-13 12:06:30 -0400

    changing

--- connection.js
+++ connection.js
@@ -14,7 +14,7 @@ let mongoURI = ""
 if(process.env.NODE_ENV === "production"){
   mongoURI = process.env.DB_URL
 } else{
-  mongoURI = 'mongodb://localhost/book-e'
+  mongoURI = 'mongodb://localhost/job'
 }
 
 mongoose
